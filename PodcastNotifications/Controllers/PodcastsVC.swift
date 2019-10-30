f4045c61d11bd253546523a7e95ff56ff38397d9
Iram Fattah <iramfattah94@gmail.com>
2019-10-30 16:54:45 -0400

    add Podcasts table view

--- PodcastNotifications/Controllers/PodcastsVC.swift
+++ PodcastNotifications/Controllers/PodcastsVC.swift
@@ -9,22 +9,64 @@
 import UIKit
 
 class PodcastsVC: UIViewController {
+    
+    //MARK: Properties
+    
+ 
+       private var currentSelectedPodCast: Podcast!
+       private var currentImage: UIImage!
+    
+       private var podcasts = [Podcast]() {
+           didSet {
+               DispatchQueue.main.async {
+                   self.tableView.reloadData()
+               }
+           }
+       }
+    
 
+    
+    //MARK: UI Objects
+    
+    lazy var tableView: UITableView = {
+           let tv = UITableView(frame: view.bounds)
+           tv.register(PodcastCell.self, forCellReuseIdentifier: "PodcastCell")
+           tv.dataSource = self
+           tv.delegate = self
+           tv.rowHeight = 80
+           return tv
+       }()
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
+    
+    //MARK: Private methods
+    
+    
 
-    /*
-    // MARK: - Navigation
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+extension PodcastsVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return podcasts.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PodcastCell", for: indexPath) as! PodcastCell
+        cell.selectionStyle = .none
+        let podcast = podcasts[indexPath.row]
+        //TODO: configure cell
+        return cell
+    }
+    
+    
 }
