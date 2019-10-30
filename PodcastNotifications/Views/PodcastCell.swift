967a42572d5b448864f5e83a041fa92fa455c257
Iram Fattah <iramfattah94@gmail.com>
2019-10-30 12:14:25 -0400

    Add podcast model and json parsing struct

---
+++ PodcastNotifications/Views/PodcastCell.swift
@@ -0,0 +1,24 @@
+//
+//  PodcastCell.swift
+//  PodcastNotifications
+//
+//  Created by C4Q on 10/30/19.
+//  Copyright © 2019 Iram Fattah. All rights reserved.
+//
+
+import UIKit
+
+class PodcastCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
